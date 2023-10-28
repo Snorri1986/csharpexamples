1b517ee018b750e3126b21a2f1ea93d8a2fa77d8
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-10-28 14:01:01 +0200

    StringBuilder

---
+++ StringBuilderExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderExample
+{
+    class Program
+    {
+          static void Main(String[] args)
+        {
+            StringBuilder sb = new StringBuilder("Random Text");
+            StringBuilder sb1 = new StringBuilder("Random Text");
+        }
+    }
+}
