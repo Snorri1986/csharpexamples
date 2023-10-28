d82e2f27a7e8d04be62d3b31413ffa746f4ce0c6
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-10-28 13:28:37 +0200

    Loops examples

---
+++ Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1,11);
+            int numberGuessed = 0;
+            Console.WriteLine("Random Num :", secretNumber);
+            
+            // do ... while loop
+            do
+            {
+                Console.WriteLine("Enter a number between 1 and 10");
+                numberGuessed = Convert.ToInt32(Console.ReadLine());
+
+            } while (secretNumber != numberGuessed);
+
+            Console.WriteLine("You guessed: {0}", secretNumber);
+        }
+    }
+}
