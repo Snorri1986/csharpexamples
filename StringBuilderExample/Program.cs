6710ae359ccdd474f1c2cf2d692d34cfa883fe66
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-11-01 21:18:34 +0100

    StringBuilder example

--- StringBuilderExample/Program.cs
+++ StringBuilderExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Text;
 
 namespace StringBuilderExample
@@ -8,7 +9,23 @@ namespace StringBuilderExample
           static void Main(String[] args)
         {
             StringBuilder sb = new StringBuilder("Random Text");
-            StringBuilder sb1 = new StringBuilder("Random Text");
+            StringBuilder sb2 = new StringBuilder("More staff that is very important",256);
+            Console.WriteLine("Capacity: {0}", sb2.Capacity);
+            Console.WriteLine("Length: {0}", sb2.Length);
+            sb2.AppendLine("\nMore important text");
+            CultureInfo enUS = CultureInfo.CreateSpecificCulture("en-US");
+            string bestCust = "Bob Smith";
+            sb2.AppendFormat(enUS, "Best Customer: {0}", bestCust);
+            Console.WriteLine(sb2.ToString());
+            sb2.Replace("text", "characters");
+            Console.WriteLine(sb2.ToString());
+            sb2.Clear();
+            sb2.Append("Random Text");
+            Console.WriteLine(sb.Equals(sb2));
+            sb2.Insert(11,"That's greate");
+            Console.WriteLine(sb2.ToString());
+            sb2.Remove(11,7);
+            Console.WriteLine(sb2.ToString());
         }
     }
 }
