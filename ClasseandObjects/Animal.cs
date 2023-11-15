93884dd3ce2f99490de6834def1d27db3c778205
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-11-15 23:10:15 +0100

    ClasseObjects

---
+++ ClasseandObjects/Animal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseandObjects
+{
+    class Animal
+    {
+        private string name;
+        private string sound;
+        public static int numOfAnimals = 0;
+        public const string SHELTER = "Derek's home for animals";
+        public readonly int idNum;
+
+        public Animal() : this("No name", "no sound") { }
+
+        public Animal(string name) : this(name, "no sound") { }
+
+        public Animal(string name, string souns)
+        {
+            SetName(name);
+            Sound = sound;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("{0} says {1}", name, sound);
+        }
+
+        public static int GetNumAnimals()
+        {
+            return numOfAnimals;
+        }
+
+        public void SetName(string name)
+        {
+            if(!name.Any(char.IsDigit))
+            {
+                this.name = name;
+            } else
+            {
+                this.name = "No name";
+                Console.WriteLine("Name can't contain numbers");
+            }
+
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        // property example
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if(value.Length > 10)
+                {
+                    sound = "No Sound";
+                    Console.WriteLine("Sound is too long");
+                } else
+                {
+                    sound = value;
+                }
+            }
+        }
+
+        public string Owner { get; set; } = "No owner";
+
+        public static int numOfAnimals
+        {
+            get { return numOfAnimals; }
+        }
+    }
+}
---
+++ ClasseandObjects/Program.cs
@@ -0,0 +1,64 @@
+﻿using ClasseandObjects;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Classes
+{
+    public class ClasseObjects
+    {
+        struct Rectangle
+        {
+            public double length;
+            public double width;
+
+            public Rectangle(double l = 1, double w = 1)
+            {
+                length = 1;
+                width = l;
+            }
+
+            public double Area()
+            {
+                return length * width;
+            }
+        }
+
+        static void Main(string[] args) {
+            // struct examples
+            Rectangle rect1;
+            rect1.length = 200;
+            rect1.width = 50;
+            Console.WriteLine("Area of rec1 : {0}", rect1.Area());
+
+            Rectangle rect2 = new Rectangle(100, 40);
+            rect2 = rect1;
+            rect1.length = 33;
+            Console.WriteLine("Rect2.length : {0}", rect2.length);
+
+            // objects examples
+            Animal fox = new Animal()
+            {
+                name = "Red",
+                sound = "Raaw"
+             };
+
+            Console.WriteLine("# of Animals {0}", Animal.GetNumAnimals());
+
+            Console.WriteLine("Area of Rectangle : {0}",ShapeMath.GetArea("Rectangel", 5,6));
+
+            // nullable types and check nulls
+            int? randNum = null;
+            
+            if(randNum == null)
+            {
+                Console.WriteLine("randNum is null");
+            }
+
+            if (!randNum.HasValue)
+            {
+                Console.WriteLine("randNum is null");
+            }
+        }
+    }
+    
+}
